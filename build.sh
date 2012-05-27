b86da97a7f0ffc7af3faf8bb5f9759787ac27f9a
George Nychis <gnychis@gmail.com>
2012-05-27 12:04:49 -0400

    removing package tree

--- build.sh
+++ build.sh
@@ -5,6 +5,3 @@ make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
 cd ../compat-wireless-2012-04-26/
 ./make.sh
 cd ../sgs2-skyrocket-kernel
-find . -name "*.ko" -exec cp {} initramfs/lib/modules \;
-#./mkbootfs initramfs | gzip > ramdisk.gz
-#./mkbootimg-sg2x --kernel zImage --ramdisk ramdisk.gz --cmdline "androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4" -o boot.img --base 0x40400000 --pagesize 2048
