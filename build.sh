5feb42552e06b81ead05188f0f7d1ca91c7e3e5d
George Nychis <gnychis@gmail.com>
2012-05-29 10:31:12 -0400

    adding in our new config with proper build for FTDI serial device support

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 make ARCH=arm clean
-make ARCH=arm msm8660_celox_usa_att_rev02_defconfig
+make ARCH=arm android_wmon_defconfig
+#make ARCH=arm msm8660_celox_usa_att_rev02_defconfig
 make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
 cd ../compat-wireless-2012-04-26/
 ./make.sh
