8d1d9442cc94c8f972ac5bc5723a54b180379237
George Nychis <gnychis@gmail.com>
2012-05-30 17:30:55 -0400

    need to add in the firmware for the ae1000

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ make ARCH=arm clean
 make ARCH=arm android_wmon_defconfig
 #make ARCH=arm msm8660_celox_usa_att_rev02_defconfig
 make -j2 ARCH=arm CROSS_COMPILE=arm-eabi-
-cd ../compat-wireless-2012-04-26/
+cd ../compat-wireless-2012-05-10/
 ./make.sh
 cd ../sgs2-skyrocket-kernel
 ./package.sh
--- package.sh
+++ package.sh
@@ -10,7 +10,7 @@ cp arch/arm/boot/zImage .
 ./mkbootimg-sg2x --kernel zImage --ramdisk ramdisk.gz --cmdline "androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4" -o boot.img --base 0x40400000 --pagesize 2048
 cp boot.img package_tree
 rm zImage
-find ../compat-wireless-2012-04-26/ -name "*.ko" -exec cp {} package_tree/system/etc/awmon_modules/ \;
+find ../compat-wireless-2012-05-10/ -name "*.ko" -exec cp {} package_tree/system/etc/awmon_modules/ \;
 cd package_tree
 zip -r ../sgs2_awmon.zip *
 cd ../
